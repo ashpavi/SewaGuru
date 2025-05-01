e1fb2f95b7dae4ec97a8077f456c46de44c37af7
AshenFernando <10899670@students.plymouth.ac.uk>
2025-05-01 16:33:59 +0530

    updateUser endpoint

--- sewaguru_backend/routes/userRouter.js
+++ sewaguru_backend/routes/userRouter.js
@@ -1,17 +1,18 @@
 import express from "express";
-import { createAdmin, login, logout, refreshToken, register, upgradeToProvider, getUserById } from "../controllers/userController.js";
+import { createAdmin, login, logout, refreshToken, register, upgradeToProvider, getLoggedInUser, updateUser } from "../controllers/userController.js";
 import { adminOnly, customerOnly } from '../middleware/accessLevel.js';
 import { authenticate } from '../middleware/auth.js';
 import { upload } from "../middleware/fileHandler.js";
 
 const router = express.Router();
 
-router.get('/', authenticate, getUserById);
+router.get('/', authenticate, getLoggedInUser);
 router.post('/register', register);
 router.post('/create-admin', authenticate, adminOnly, createAdmin);
 router.post('/login', login);
 router.post('/refresh', refreshToken);
 router.post('/logout', authenticate, logout);
+router.put('/update', authenticate, updateUser);
 
 router.post('/upgrade',
   authenticate,
