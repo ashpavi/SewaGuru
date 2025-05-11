71c8a99c855137a072e5b9f516be7ba45fdb5d85
AshenFernando <10899670@students.plymouth.ac.uk>
2025-05-11 17:31:31 +0530

    admin views

--- sewaguru_backend/routes/userRouter.js
+++ sewaguru_backend/routes/userRouter.js
@@ -1,17 +1,23 @@
 import express from "express";
-import { createAdmin, login, logout, refreshToken, register, upgradeToProvider, getLoggedInUser, updateUser } from "../controllers/userController.js";
+import { test } from '../controllers/test.js';
+import { createAdmin, getAll, getLoggedInUser, login, logout, refreshToken, register, toggleUserStatus, updateUser, upgradeToProvider } from "../controllers/userController.js";
 import { adminOnly, customerOnly } from '../middleware/accessLevel.js';
 import { authenticate } from '../middleware/auth.js';
 import { upload } from "../middleware/fileHandler.js";
-import {test} from '../controllers/test.js';
 
 const router = express.Router();
 
 router.get('/test', test)
 
 router.get('/', authenticate, getLoggedInUser);
+router.get('/all/:role', authenticate, adminOnly, getAll);
+router.post('/create-admin', authenticate, adminOnly, createAdmin);
+router.post('/login', login);
+router.post('/refresh', refreshToken);
+router.post('/logout', authenticate, logout);
+router.patch('/:userId/status', authenticate, adminOnly, toggleUserStatus);
 
-router.post('/register',upload.fields([
+router.post('/register', upload.fields([
   { name: 'nicImages', maxCount: 2 },
   { name: 'profileImage', maxCount: 1 },
   { name: 'gsCerts', maxCount: 1 },
@@ -19,11 +25,7 @@ router.post('/register',upload.fields([
   { name: 'extraCerts' },
 ]), register);
 
-router.post('/create-admin', authenticate, adminOnly, createAdmin);
-router.post('/login', login);
-router.post('/refresh', refreshToken);
-router.post('/logout', authenticate, logout);
-router.put('/update', authenticate,upload.fields([
+router.put('/update', authenticate, upload.fields([
   { name: 'nicImages', maxCount: 2 },
   { name: 'profileImage', maxCount: 1 },
   { name: 'gsCerts', maxCount: 1 },
---
+++ sewaguru_frontend/src/pages/admin/registeredCustomers.jsx
@@ -0,0 +1,96 @@
+import { useEffect, useRef, useState } from "react";
+import toast from "react-hot-toast";
+import api from "../../api/api";
+import ListItem from "../../components/listItem";
+import Loader from "../../components/loader";
+
+export default function AdminRegisteredCustomers() {
+  const hasFetched = useRef(false);
+
+  const [loading, setLoading] = useState(true);
+  const [customersList, setCustomersList] = useState([]);
+
+  useEffect(() => {
+    if (!hasFetched.current) {
+      fetchUsers();
+      hasFetched.current = true;
+    }
+  }, []);
+
+  useEffect(() => {
+    console.log(customersList)
+  }, [customersList]);
+
+
+  const fetchUsers = async () => {
+    setLoading(true);
+    try {
+      const token = localStorage.getItem("accessToken");
+      const response = await api.get("/user/all/customer", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      setCustomersList(response.data);
+    } catch (error) {
+      console.error("Error fetching profile:", error);
+      toast.error("Failed to fetch profile data.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const changeUserStatus = async (id, isDisabled) => {
+    setLoading(true);
+
+    try {
+      const token = localStorage.getItem("accessToken");
+      await api.patch(`/user/${id}/status`,
+        { isDisabled },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        },
+      );
+
+      // Update local state to reflect new status
+      setCustomersList((prevList) =>
+        prevList.map((user) =>
+          user.id === id ? { ...user, isDisabled } : user
+        )
+      );
+
+      toast.success(`User has been ${isDisabled ? "disabled" : "enabled"}.`);
+    } catch (e) {
+      console.error("Error changing User Status:", e);
+      toast.error("Failed to change user Status.");
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  if (loading) {
+    return <Loader />;
+  }
+
+  return (
+    <div className="m-2">
+      <h2 className="text-xl font-bold mb-6 text-center text-gray-800">Registered Customers</h2>
+
+      <ul className="space-y-2">
+        {customersList.map((customer) => (
+          <ListItem
+            key={customer.id}
+            name={`${customer.firstName} ${customer.lastName}`}
+            registeredOn={`${customer.createdAt}`}
+            disabled={customer.isDisabled}
+            onAccept={() => changeUserStatus(customer.id, false)}
+            onDeny={() => changeUserStatus(customer.id, true)}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+}
