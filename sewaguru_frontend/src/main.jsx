672e2b65fd62790bbea5e73f32936435a48dce7e
Ashen Fernando <104290507+ashpavi@users.noreply.github.com>
2025-08-15 21:15:43 +0530

    Update main.jsx

--- sewaguru_frontend/src/main.jsx
+++ sewaguru_frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
-// Let Vite bundle Flowbite instead of <script> in index.html
+
 import "flowbite"; // or: import "flowbite/dist/flowbite.min.js";
 
 createRoot(document.getElementById("root")).render(
