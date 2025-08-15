a10f2468fcada642eb74aa0e7825a85eabc3b17f
AshenFernando <ashenpavithra@gmail.com>
2025-08-15 18:14:35 +0530

    fixes

--- sewaguru_frontend/src/main.jsx
+++ sewaguru_frontend/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+// Let Vite bundle Flowbite instead of <script> in index.html
+import "flowbite"; // or: import "flowbite/dist/flowbite.min.js";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
